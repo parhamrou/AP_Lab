474fe058ce5721b0c396b5c710971a0be17a36cf
Parham Roufarshbaf <p_roufarshbaf@yahoo.com>
2022-02-15 21:44:17 +0330

    feat: input part is added

---
+++ lesson1/Calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner();
+
+	System.out.println("Enter first number, operator and 2nd number");
+	System.out.println("exp: 9 * 8");
+	int num1 = input.nextInt();
+	char op = input.next().charAt(0);
+	int num2 = input.nextInt();
+
+
+    }
+}
