e9de898d148f4174ce350106cbfae4d038bcef09
Parham Roufarshbaf <p_roufarshbaf@yahoo.com>
2022-02-15 21:47:31 +0330

    feat: calculation part is added

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -9,6 +9,25 @@ public class Calculator {
 	int num1 = input.nextInt();
 	char op = input.next().charAt(0);
 	int num2 = input.nextInt();
+        int result;
+
+        switch(op) {
+	    case '+':
+		result = num1 + num2;
+		breakl;
+	    case '-':
+		result = num1 - num2;
+		break;
+	    case '*':
+		result = num1 * num2;
+		break;
+	    case '/':
+		result = num1 / num2;
+		break;
+	    default:
+		System.out.println("Invalid input");
+		return;
+	}
 
 
     }
